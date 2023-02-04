07b3b46baa1eb6534cbc3b882078f7fa15e62008
kumar-manish000004 <124406146+kumar-manish000004@users.noreply.github.com>
2023-02-04 07:05:36 +0530

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,81 @@
+module.exports = {
+  siteTitle: 'Maneesh Kumar Sinha | Software Developer ',
+  siteDescription:
+    'Maneesh Sinha is a Upcoming Software Developer, who loves learning new things and helping tech beginners.',
+  siteKeywords:
+    'Maneesh Kumar Sinha, Maneesh Kumar, kumar_manish04, kumar_manish,kumar_manish04, software developer, software engineer, face mask, face mask detection, jamshedpur, LPU, Lovely Proffesional University',
+  siteUrl: 'https://github.com/KUMAR-MANISH04',
+  siteLanguage: 'en_US',
+  googleAnalyticsID: 'UA-45666519-2',
+  googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
+  name: 'Maneesh Kumar Sinha',
+  location: 'Jamshedpur, India',
+  email: '7050maneesh@gmail.com',
+  github: 'https://github.com/KUMAR-MANISH04',
+  twitterHandle: 'MANEESH84765536',
+  socialMedia: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/KUMAR-MANISH04',
+    },
+    {
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/mk-sinha/',
+    },
+    {
+      name: 'Leetcode',
+      url: 'https://leetcode.com/kumar_manish04/',
+    },
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/kumar_manish04',
+    },
+    {
+      name: 'Twitter',
+      url: 'https://twitter.com/MANEESH84765536',
+    },
+  ],
+
+  navLinks: [
+    {
+      name: 'About',
+      url: '/#about',
+    },
+    {
+      name: 'Education',
+      url: '/#jobs',
+    },
+    {
+      name: 'Projects',
+      url: '/#projects',
+    },
+    {
+      name: 'Contact',
+      url: '/#contact',
+    },
+  ],
+
+  navHeight: 100,
+
+  colors: {
+    green: '#64ffda',
+    navy: '#0a192f',
+    darkNavy: '#020c1b',
+  },
+
+  srConfig: (delay = 200) => ({
+    origin: 'bottom',
+    distance: '20px',
+    duration: 500,
+    delay,
+    rotate: { x: 0, y: 0, z: 0 },
+    opacity: 0,
+    scale: 1,
+    easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+    mobile: true,
+    reset: false,
+    useDelay: 'always',
+    viewFactor: 0.25,
+    viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+  }),
+};
